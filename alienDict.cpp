02fa2ea2832f88967dfbd228e74fb8cba19f7588
jyothsnashaji <jyothsnashaji99@gmail.com>
2020-04-18 22:43:38 +0530

    seg fault. Topological sorting of graph

--- alienDict.cpp
+++ alienDict.cpp
@@ -1,3 +1,16 @@
+/*Given a sorted dictionary of an alien language having N words and k starting alphabets of standard dictionary the task is to complete the function which returns a string denoting the order of characters in the language.
+Note: Many orders may be possible for a particular test case, thus you may return any valid order.
+Examples:
+Input:  Dict[ ] = { "baa", "abcd", "abca", "cab", "cad" }, k = 4
+Output: Function returns "bdac"
+Here order of characters is 'b', 'd', 'a', 'c'
+Note that words are sorted and in the given language "baa"
+comes before "abcd", therefore 'b' is before 'a' in output.
+Similarly we can find other orders.
+
+Input: Dict[] = { "caa", "aaa", "aab" }, k = 3
+Output: Function returns "cab"*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
