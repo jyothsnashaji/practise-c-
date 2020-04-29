e74627936583a353854b17b58cc3e557e4789580
jyothsnashaji <jyothsnashaji99@gmail.com>
2020-04-29 23:51:27 +0530

    not opt

--- t.cpp
+++ t.cpp
@@ -1,92 +1,64 @@
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Graph
+int minTimeOpt(vector<int> a, int start, int p)
 {
-    int v;
-    set<int> adj[26];
+    if (p == 1)
+        return accumulate(a.begin() + start, a.end(), 0);
+    int n = a.size();
+    vector<vector<int>> sum(n, vector<int>(n, 0));
+    vector<vector<int>> ans(n, vector<int>(p + 1, INT16_MAX));
 
-public:
-    Graph(int V)
+    for (int i = 0; i < n; ++i)
     {
-        this->v = V;
-        //adj= new vector <set <int>>[V];
-    }
-
-    void addEdge(int u, int v)
-    {
-        adj[u].insert(v);
-    }
-    void printEdges()
-    {
-        for (int i = 0; i < v; ++i)
-        {
-            cout << "\n"
-                 << i << "..";
-            for (auto j = adj[i].begin(); j != adj[i].end(); ++j)
-                cout << *j << " ";
-        }
-    }
-
-    void topologicalSortRecursive(int v, vector<bool> &visited, stack<int> &s)
-    {
-        visited[v] = true;
-        for (auto i = adj[v].begin(); i != adj[v].end(); ++i)
-        {
-            if (!visited[*i])
-                topologicalSortRecursive(*i, visited, s);
-        }
-        s.push(v);
-    }
-
-    string topologicalSort()
-    {
-        stack<int> s;
-        vector<bool> visited(v, false);
-        for (int i = 0; i < v; ++i)
-        {
-            if (!visited[i])
-                topologicalSortRecursive(i, visited, s);
-        }
-        char str[v];
-            for(int i=0;i<v;++i)
-        {
-            str[i] = s.top() + 'a';
-            //cout << s.top() << " ";
-            s.pop();
-        }
-        return str;
-    }
-};
-string order(string dict[], int n, int k)
-{
-    Graph g(k);
-    for (int i = 0; i < n - 1; ++i)
-    {
+        sum[i][i] = a[i];
         for (int j = i + 1; j < n; ++j)
+            sum[i][j] = sum[i][j - 1] + a[j];
+    }
+    for (int i = n - 1; i >= 0; --i)
+    {
+        ans[i][1] = sum[i][n - 1];
+        for (int j = 2; j <= p; ++j)
         {
-            if (dict[i] != dict[j])
+           // cout << i << " " << j << "//";
+            for (int k = i; k < n; ++k)
             {
-                auto mis = mismatch(dict[i].begin(), dict[i].end(), dict[j].begin());
-                g.addEdge(*mis.first - 'a', *mis.second - 'a');
-                //cout << *mis.first - 'a' << " " << *mis.second - 'a' << "\n";
+                ans[i][j] = min(ans[i][j], max(sum[0][k - 1], ans[k][j - 1]));
+                //cout << i << " " << j << " " << k << "..";
             }
         }
     }
-    //g.printEdges();
-    return g.topologicalSort();
+    return ans[0][p];
+}
+
+int minTime(vector<int> a, int start, int p)
+{
+    if (!(start <= a.size() && p))
+        return 0;
+    if (p == 1)
+        return accumulate(a.begin() + start, a.end(), 0);
+    int mint = INT16_MAX;
+    for (int i = start; i <= a.size(); ++i)
+        mint = min(mint, max(accumulate(a.begin() + start, a.begin() + i + 1, 0), minTime(a, i + 1, p - 1)));
+    //cout<<start<<" "<<end<<" "<<p<<" "<<mint<<"..";
+    return mint;
 }
 
 int main()
 {
-    int t, n, k;
+    int t, n, p;
+
     cin >> t;
     while (t--)
     {
-        cin >> n >> k;
-        string dict[n];
+        cin >> p >> n;
+        vector<int> a(n);
         for (int i = 0; i < n; ++i)
-            cin >> dict[i];
-        cout << order(dict, n, k) << "\n";
+            cin >> a[i];
+        //cout << dfs(s1, s2) << "\n";
+        cout << minTimeOpt(a, 0, p) << "\n";
     }
+    return 0;
 }
